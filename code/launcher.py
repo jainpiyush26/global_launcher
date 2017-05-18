8f8eeb80ed25ae1ac90e920111de6c5184a97745
Piyush Jain <piyush@Piyushs-MBP.hitronhub.home>
2017-05-17 23:19:17 -0700

    Fixed the logic for the drag and drop, working on making the translucent object a little better

--- code/launcher.py
+++ code/launcher.py
@@ -4,6 +4,7 @@ from PySide.QtGui import *
 import os
 import sys
 import json
+from subprocess import Popen
 from collections import defaultdict
 
 # Generate the resource path
@@ -11,27 +12,37 @@ RESOURCES_PATH = os.path.join(os.path.dirname(__file__), "../", "resources")
 
 
 class dropDialog(QDialog):
+    """
+    Launches a small dialog for dragging and dropping the application's inputs
+    """
     def __init__(self, parent=None):
         super(dropDialog, self).__init__(parent=None)
-        self.parent_geometry = parent.system_tray_icon.geometry()
-        self.setGeometry(QRect(self.parent_geometry.x(), self.parent_geometry.y() + 110, 100, 100))
-
-        self.layout = QGridLayout()
+        self.parent = parent
+        self.parent_geometry = self.parent.system_tray_icon.geometry()
+        if sys.platform == "darwin":
+            self.setGeometry(QRect(self.parent_geometry.x(), self.parent_geometry.y() + 30, 60, 60))
+        else:
+            self.setGeometry(QRect(self.parent_geometry.x(), self.parent_geometry.y() + 110, 60, 60))
+        self.setWindowFlags(Qt.SplashScreen | Qt.WindowStaysOnTopHint)
+        # self.setAttribute(Qt.WA_TintedBackground)
+        self.setStyleSheet("background-color: rgba(255, 255, 255, 100);")
+        self.layout = QHBoxLayout()
         self.label = QLabel(self)
-        pixmap = QImage(os.path.join(self.parent.icon_path, "drop.png"))
-        self.label.setPixmap(pixmap)
-
-        self.layout.addWidget(pixmap)
+        pixmap = QPixmap(os.path.join(self.parent.icon_path, "drop.png"))
+        self.label.setPixmap(pixmap.scaled(50, 50, Qt.KeepAspectRatio))
+        self.layout.addWidget(self.label)
         self.setLayout(self.layout)
         self.setAcceptDrops(True)
-
 
     def dragEnterEvent(self, event):
         event.accept()
 
     def dropEvent(self, event):
-        event.accept()
-        self.parent.drop_data = str(event.mimedata().urls[0].toLocalFile())
+        self.parent.drop_data = str(event.mimeData().urls()[0].toLocalFile())
+        self.close()
+
+    def mouseDoubleClickEvent(self, event):
+        self.parent.drop_data = None
         self.close()
 
 
@@ -59,9 +70,7 @@ class GlobalLauncher(QDialog):
 
     def setup_system_tray_icon(self):
         self.global_launcher_main_menu = QMenu()
-
         self.prepare_app_actions()
-
         self.close_action = QAction("Close", self, triggered=self.close)
         self.close_action.setIcon(QIcon(os.path.join(self.icon_path, "close.png")))
         self.global_launcher_main_menu.addAction(self.close_action)
@@ -101,10 +110,16 @@ class GlobalLauncher(QDialog):
         sender_app_name = str(self.sender().text())
         application_location = self.app_action_dict[sender_app_name][0]
         application_accept_drop = self.app_action_dict[sender_app_name][1]
-        print application_location, application_accept_drop
-
+        if application_accept_drop:
+            drop_ui = dropDialog(self)
+            drop_ui.exec_()
+        if self.drop_data:
+            Popen("{} {}".format(application_location, self.drop_data), shell=True)
+        else:
+            Popen("{}".format(application_location), shell=True)
 
     def closeEvent(self, event):
+        print "closing system tray"
         event.accept()
         sys.exit()
 
